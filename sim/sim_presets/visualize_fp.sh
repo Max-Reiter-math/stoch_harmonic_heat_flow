55a82758527461569ea287fa12d5d48b1fc6ac18
Max Reiter (WSL) <87012332+Max-Reiter-math@users.noreply.github.com>
2026-07-20 10:34:15 +0200

    added for comparison

---
+++ sim/sim_presets/visualize_fp.sh
@@ -0,0 +1,7 @@
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid fp75 -lam 1 -dh 11 -dt 0.005 -e unstable -m fp_coupled -cs 1 -seed 75 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid fp33 -lam 1 -dh 11 -dt 0.005 -e unstable -m fp_coupled -cs 1 -seed 33 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid fp141 -lam 1 -dh 11 -dt 0.005 -e unstable -m fp_coupled -cs 1 -seed 141 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid fp104 -lam 1 -dh 11 -dt 0.005 -e unstable -m fp_coupled -cs 1 -seed 104 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid fp202 -lam 1 -dh 11 -dt 0.005 -e unstable -m fp_coupled -cs 1 -seed 202 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid fp102 -lam 1 -dh 11 -dt 0.005 -e unstable -m fp_coupled -cs 1 -seed 102 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid fp207 -lam 1 -dh 11 -dt 0.005 -e unstable -m fp_coupled -cs 1 -seed 207 
