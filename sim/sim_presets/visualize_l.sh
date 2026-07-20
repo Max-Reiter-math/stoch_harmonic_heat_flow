c51fae3b63037a923d49a896234bd5c2fc50163f
Max Reiter (WSL) <87012332+Max-Reiter-math@users.noreply.github.com>
2026-07-20 09:42:09 +0200

    correction

---
+++ sim/sim_presets/visualize_l.sh
@@ -0,0 +1,7 @@
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l75 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 75 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l33 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 33 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l141 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 141 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l104 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 104 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l202 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 202 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l102 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 102 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l207 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 207 
