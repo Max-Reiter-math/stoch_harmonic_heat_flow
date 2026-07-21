2d80f43bfb561ef9146e08a3cfc261551b35feb2
Max Reiter <87012332+Max-Reiter-math@users.noreply.github.com>
2026-07-21 08:48:53 +0200

    Update sim preset

--- sim/sim_presets/visualize_l.sh
+++ sim/sim_presets/visualize_l.sh
@@ -1,7 +1,9 @@
-python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l75 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 75 || true
-python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l33 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 33 || true
-python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l141 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 141 || true
-python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l104 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 104 || true
-python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l202 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 202 || true
-python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l102 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 102 || true
-python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -sid l207 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 207 
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 125 -sid l125 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 96 -sid l96 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 175 -sid l175 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 169 -sid l169 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 147 -sid l147 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 174 -sid l174 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 145 -sid l145 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 23 -sid l23 || true
+python -m sim.run -vtx 1 -fsr 0.01 -msr 0.01 -ovw 1 -lam 1 -dh 11 -dt 0.005 -e unstable -m linear_cg -cs 1 -seed 78 -sid l78
